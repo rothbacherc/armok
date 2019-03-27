63c8190b8b16733c134a9f3e43d76e361dadba13
c_rothbacher <crothbacher@peerassist.com>
2019-03-27 16:11:26 -0400

    Framed landing page

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+/* Color Schemes:
+Light shade: Foggy Gray: #CFCABB
+Light accent: Sirocco: #697C76
+Main brand: Cape Cod: #43426
+Dark accent: Blue Bayoux: #4E6877
+Dark shades: Heavy Metal: #141A16
+*/
+
 if (environment.production) {
   enableProdMode();
 }
